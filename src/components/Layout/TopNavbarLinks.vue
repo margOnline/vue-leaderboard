b22643ed982b1165b5a829f33f1032f170e8eb49
Margo Urey <margo@margonline.co.uk>
2024-07-18 16:32:15 +0100

    Extract navbar links into reusable component (#12)

---
+++ src/components/Layout/TopNavbarLinks.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+interface linkProp {
+  title: string
+  to: string
+}
+
+defineProps<{
+  links: linkProp[]
+}>()
+</script>
+
+<template>
+  <li v-for="link in links" :key="link.title">
+    <RouterLink :to="link.to">{{ link.title }}</RouterLink>
+  </li>
+</template>
+
+<style scoped>
+li {
+  display: inline;
+  list-style-type: none;
+  border: none;
+  padding-left: 10px;
+  text-transform: uppercase;
+  font-weight: bold;
+  padding: 0 10px;
+  background-position: left center;
+  :hover,
+  :active {
+    color: #13b56f;
+  }
+  a {
+    color: #969696;
+    text-decoration: none;
+  }
+}
+</style>
