de3e2ef4223585a48ab0c42b70e2d6e31423fa77
Margo Urey <margo@margonline.co.uk>
2024-07-19 16:09:01 +0100

    Initial page for submitting an entry

---
+++ src/pages/submit-entry.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+</script>
+
+<template>
+  <div class="submit-entry-container">
+    <div class="flex flex col gap-2 ml-5 mt-5">
+      <img src="../assets/images/icons/submit.svg" />
+      <h2 class="text-2xl">Submit Entry</h2>
+    </div>
+
+    <div class="submit-entry-form-container"></div>
+  </div>
+</template>
