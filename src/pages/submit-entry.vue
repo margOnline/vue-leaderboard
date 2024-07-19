3cbbcfc631fa0d6f1efeb61038ad3f069964e07c
Margo Urey <margo@margonline.co.uk>
2024-07-19 17:10:17 +0100

    Create a reusable PageTitle component
    Add a favicon and amend site title

--- src/pages/submit-entry.vue
+++ src/pages/submit-entry.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
+import PageTitle from '@/components/PageTitle.vue'
 import { ref } from 'vue'
 </script>
 
 <template>
   <div class="submit-entry-container">
-    <div class="flex flex col gap-2 ml-5 mt-5">
-      <img src="../assets/images/icons/submit.svg" />
-      <h2 class="text-2xl">Submit Entry</h2>
-    </div>
-
+    <PageTitle :iconFile="'submit.svg'" :title="'Submit Entry'" />
     <div class="submit-entry-form-container"></div>
   </div>
 </template>
