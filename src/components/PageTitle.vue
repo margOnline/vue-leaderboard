c41922f53483a7a24fa073d6d1b0b4e37bd2eed6
Margo Urey <margo@margonline.co.uk>
2024-07-20 20:54:28 +0100

    Include a seeded players box and styling improvements

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -6,7 +6,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="flex gap-2 ml-5 mt-5">
+  <div class="flex gap-2 ml-5 mt-5 mb-5">
     <img :src="`../src/assets/images/icons/${iconFile}`" />
     <h2 class="text-2xl">{{ title }}</h2>
   </div>
